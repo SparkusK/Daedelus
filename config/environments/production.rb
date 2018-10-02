12b025cc98e441aa115bdaa51ce72f2f86c2a539
Shane Kelly <shane.sparky.kelly@gmail.com>
2018-10-02 21:37:44 +0200

    Add host to mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
  # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'daedelus.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
