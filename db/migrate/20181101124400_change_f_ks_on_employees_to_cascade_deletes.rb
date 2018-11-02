b76c164b66a566e2a21edcb0403ab7ff53f2a26f
Shane Kelly <shane.sparky.kelly@gmail.com>
2018-11-02 16:13:52 +0200

    Change Employee referenced-by tables to delete on cascade

---
+++ db/migrate/20181101124400_change_f_ks_on_employees_to_cascade_deletes.rb
@@ -0,0 +1,15 @@
+class ChangeFKsOnEmployeesToCascadeDeletes < ActiveRecord::Migration[5.1]
+  def up
+    remove_foreign_key :managers, :employees
+    remove_foreign_key :labor_records, :employees
+    add_foreign_key :labor_records, :employees, :on_delete: :cascade
+    add_foreign_key :managers, :employees, :on_delete: :cascade
+  end
+
+  def down
+    remove_foreign_key :labor_records, :employees
+    remove_foreign_key :managers, :employees
+    add_foreign_key :managers, :employees
+    add_foreign_key :labor_records, :employees
+  end
+end
