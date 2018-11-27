fcf2fb38fe52772e156124e08b44e86f31268a89
Shane Kelly <shane.sparky.kelly@gmail.com>
2018-11-27 14:07:38 +0200

    Optimize Sections performance for get_removal_confirmation

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,55 +5,109 @@ class Section < ApplicationRecord
   has_many :labor_records
   has_one :manager
 
-  def get_removal_confirmation
+  # sections
+  # 	-> jobs
+  # 		-> creditor_orders
+  # 			-> credit_notes
+  # 		-> debtor_orders
+  # 			-> debtor_payments
+  # 		-> labor_records
+  # 	-> employees
+  # 		-> managers
+  # 		-> labor_records
+  # 	-> managers
+  # 	-> labor_records
+
+
+  def get_jobs(section_id)
+    Job.where(section_id: section_id)
+  end
+
+  def get_creditor_orders(job_ids)
+    CreditorOrder.where("creditor_orders.job_id IN (?)", job_ids)
+  end
+
+  def get_credit_notes(creditor_order_ids)
+    CreditNote.where("creditor_order_id IN (?)", creditor_order_ids)
+  end
+
+  def get_debtor_orders(job_ids)
+    DebtorOrder.where("job_id IN (?)", job_ids)
+  end
+
+  def get_debtor_payments(debtor_order_ids)
+    DebtorPayment.where("debtor_order_id IN (?)", debtor_order_ids)
+  end
+
+  def get_job_labor_records(job_ids)
+    LaborRecord.where("job_id IN (?)", job_ids)
+  end
+
+  def get_employees(section_id)
+    Employee.where(section_id: section_id)
+  end
+
+  def get_employee_managers(employee_ids)
+    Manager.where("employee_id IN (?)", employee_ids)
+  end
+
+  def get_employee_labor_records(employee_ids)
+    LaborRecord.where("employee_id IN (?)", employee_ids)
+  end
+
+  def get_manager(section_id)
+    Section.find_by(id: section_id).manager
+  end
+
+  def get_section_labor_records(section_id)
+    LaborRecord.where(section_id: section_id)
+  end
+
+  def get_entities(section_id)
+    jobs                    = get_jobs(                   section_id          )
+    creditor_orders         = get_creditor_orders(        jobs.ids            )
+    credit_notes            = get_credit_notes(           creditor_orders.ids )
+    debtor_orders           = get_debtor_orders(          jobs.ids            )
+    debtor_payments         = get_debtor_payments(        debtor_orders.ids   )
+    job_labor_records       = get_job_labor_records(      jobs.ids            )
+    employees               = get_employees(              section_id          )
+    employee_managers       = get_employee_managers(      employees.ids       )
+    employee_labor_records  = get_employee_labor_records( employees.ids       )
+    manager                 = get_manager(                section_id          )
+    section_labor_records   = get_section_labor_records(  section_id          )
+
+    manager.nil? ? manager_count = 0 : manager_count = 1
+    {
+      jobs: jobs,
+      creditor_orders: creditor_orders,
+      credit_notes: credit_notes,
+      debtor_orders: debtor_orders,
+      debtor_payments: debtor_payments,
+      job_labor_records: job_labor_records,
+      employees: employees,
+      employee_managers: employee_managers,
+      employee_labor_records: employee_labor_records,
+      manager_count: manager_count,
+      section_labor_records: section_labor_records
+    }
+
+  end
+
+  def get_removal_confirmation(section_id)
+    entities = get_entities(section_id)
+
     confirmation = "Performing this removal will also delete: \n"
-
-    unless self.jobs.nil?
-      confirmation << "* #{self.jobs.count} Job records, including: \n"
-      job_labor_records = 0
-      job_creditor_orders = 0
-      job_debtor_orders = 0
-
-      job_creditor_orders_payments = 0
-      job_debtor_orders_payments = 0
-
-      self.jobs.each do |job|
-        job_labor_records += job.labor_records.count unless job.labor_records.nil?
-        unless job.creditor_orders.nil?
-          job_creditor_orders += job.creditor_orders.count
-          job.creditor_orders.each do |creditor_order|
-            job_creditor_orders_payments += creditor_order.credit_notes.count unless creditor_order.credit_notes.nil?
-          end
-        end
-
-        unless job.debtor_orders.nil?
-          job_debtor_orders += job.debtor_orders.count
-          job.debtor_orders.each do |debtor_order|
-            job_debtor_orders_payments += debtor_order.debtor_payments.count unless debtor_order.debtor_payments.nil?
-          end
-        end
-      end
-
-      confirmation << "  * #{job_labor_records} Labor Records from Jobs \n"
-      confirmation << "  * #{job_creditor_orders} Creditor Order records from Jobs, including: \n"
-      confirmation << "    * #{job_creditor_orders_payments} Creditor Payment records from Creditor Orders \n"
-      confirmation << "  * #{job_debtor_orders} Debtor Order records from Jobs, including: \n"
-      confirmation << "    * #{job_debtor_orders_payments} Debtor Payment records from Debtor Orders \n"
-
-    end
-    unless self.employees.nil?
-      confirmation << "* #{self.employees.count} Employee records, including: \n"
-      employee_labor_records_count_total = 0
-      employee_managers_count_total = 0
-      self.employees.each do |employee|
-        employee_labor_records_count_total += employee.labor_records.count unless employee.labor_records.nil?
-      end
-      confirmation << "  * #{employee_labor_records_count_total} Labor Records from the Employees \n"
-    end
-    confirmation << "* 1 Manager record \n" unless self.manager.nil?
-    confirmation << "* #{self.labor_records.count} Other Labor Records \n" unless self.labor_records.nil?
-    confirmation << "Are you sure?"
-    
+    confirmation << "* #{entities[:jobs].count} Job records \n"
+    confirmation << "    * #{entities[:creditor_orders].count} Creditor Order records \n"
+    confirmation << "        * #{entities[:credit_notes].count} Creditor Payment records \n"
+    confirmation << "    * #{entities[:debtor_orders].count} Debtor Order records \n"
+    confirmation << "        * #{entities[:debtor_payments].count} Debtor Payment records \n"
+    confirmation << "    * #{entities[:job_labor_records].count} Labor Records related to Jobs \n"
+    confirmation << "* #{entities[:employees].count} Employee records \n"
+    confirmation << "    * #{entities[:employee_managers].count} Manager records \n"
+    confirmation << "    * #{entities[:employee_labor_records].count} Labor Records related to Employees \n"
+    confirmation << "* #{entities[:manager_count]} Manager records \n"
+    confirmation << "* #{entities[:section_labor_records].count} Labor Records related to the Section \n"
   end
 
   def section_name
